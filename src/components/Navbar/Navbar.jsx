d598f230af661140bd03c29e3abccd7612343897
Tan Tran <tan.tranminh0708@gmail.com>
2024-04-06 13:25:55 +0700

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,39 @@
 import React from 'react'
 import "./Navbar.css"
-import {ShoppingCartOutlined, 
-        SearchOutlined, 
-        CameraAlt, 
-        KeyboardArrowDown,
-        ArrowRightAlt 
-    } from '@mui/icons-material';
+import {
+    ShoppingCartOutlined,
+    SearchOutlined,
+    CameraAlt,
+    KeyboardArrowDown,
+    ArrowRightAlt
+} from '@mui/icons-material';
 
 const Navbar = () => {
     return (
-        <div className= 'navbar'>
-            <div className='wrapper'>
-        <div className = 'left'>
-            <img src = '/logo.png' />
-        </div>
-        <div className = 'center'></div>
-        <SearchOutlined/>
-        <div className = 'right'>
-            <ShoppingCartOutlined/>
-        </div>
+        <div className='navbar'>
+            <div className='left'>
+                <img src='/logo.png' />
+            </div>
+            <div className='center'>
+                
+                <div className="Search">
+                    <input className="searchContainer" placeholder="Search">
+                    </input>
+                    <button className = "button">
+                        <SearchOutlined style={{ color: 'white' }}  />
+                    </button>
+                    <button className = "button">
+                        <CameraAlt style={{ color: 'white' }} />
+                    </button>
+                </div>
+            </div>
 
-        </div>
+            <div className='right'>
+                <button className = "cartButton">
+                <ShoppingCartOutlined />
+                <span className = "cartCount">0</span>
+                </button>
+            </div>
         </div>
     )
 }
